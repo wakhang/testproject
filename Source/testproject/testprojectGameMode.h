d0010ffedc6a66421f5081670a279d2d7ea12bfa
wakhang <130204784+wakhang@users.noreply.github.com>
2023-06-22 22:43:52 +0900

    Initial commit

---
+++ Source/testproject/testprojectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "testprojectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AtestprojectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AtestprojectGameMode();
+};
+
+
+
